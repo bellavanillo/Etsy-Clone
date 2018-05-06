6a536e28edf58228a8246daa5e4170e4a9883362
bv <bellavanillo@gmail.com>
2018-05-06 14:59:02 -0700

    add art component

---
+++ src/app/Art&Collectibles/art.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Art } from '../art.model';
+
+@Component({
+  selector: 'app-art',
+  templateUrl: './art.component.html',
+  styleUrls: ['./art.component.css']
+})
+export class ArtComponent implements OnInit {
+
+  }
+
+
+}
