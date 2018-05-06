587fefed62c8c4a88ad77386037bfeb6c52b7c55
bv <bellavanillo@gmail.com>
2018-05-06 15:24:39 -0700

    add vintage component

---
+++ src/app/Vintage/vintage.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Vintage } from '../vintage.model';
+
+@Component({
+  selector: 'app-vintage',
+  templateUrl: './vintage.component.html',
+  styleUrls: ['./vintage.component.css']
+})
+export class VintageComponent implements OnInit {
+
+  }
+
+
+}
