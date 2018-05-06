24af8d78b22e3f464599a2bb1cb2ce3600dbf7a4
bv <bellavanillo@gmail.com>
2018-05-06 15:24:12 -0700

    add toy component

---
+++ src/app/Toys&Entertainment/toy.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Toy } from '../toy.model';
+
+@Component({
+  selector: 'app-toy',
+  templateUrl: './toy.component.html',
+  styleUrls: ['./toy.component.css']
+})
+export class ToyComponent implements OnInit {
+
+  }
+
+
+}
