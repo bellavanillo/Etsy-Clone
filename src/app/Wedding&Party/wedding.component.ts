6c1e2e18de480f5095020d8e1d93a2801161e00b
bv <bellavanillo@gmail.com>
2018-05-06 15:25:16 -0700

    add wedding component

---
+++ src/app/Wedding&Party/wedding.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Wedding } from '../wedding.model';
+
+@Component({
+  selector: 'app-wedding',
+  templateUrl: './wedding.component.html',
+  styleUrls: ['./wedding.component.css']
+})
+export class WeddingComponent implements OnInit {
+
+  }
+
+
+}
