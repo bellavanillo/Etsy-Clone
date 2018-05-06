ac07c244ae77d0f3000f9c43119f81d4d340c8d9
bv <bellavanillo@gmail.com>
2018-05-06 15:06:48 -0700

    add craft component

---
+++ src/app/CraftSupplies&Tools/craft.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Craft } from '../craft.model';
+
+@Component({
+  selector: 'app-craft',
+  templateUrl: './craft.component.html',
+  styleUrls: ['./craft.component.css']
+})
+export class CraftComponent implements OnInit {
+
+  }
+
+
+}
