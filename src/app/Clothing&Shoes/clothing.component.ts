17e3816ecdbcf643095b4078c44873a116130c7b
bv <bellavanillo@gmail.com>
2018-05-06 15:03:07 -0700

    add clothing component

---
+++ src/app/Clothing&Shoes/clothing.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Clothing } from '../clothing.model';
+
+@Component({
+  selector: 'app-clothing',
+  templateUrl: './clothing.component.html',
+  styleUrls: ['./clothing.component.css']
+})
+export class ClothingComponent implements OnInit {
+
+  }
+
+
+}
