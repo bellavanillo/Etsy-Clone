9215de8c2211e04e7613c6701fdaa832cddbe121
bv <bellavanillo@gmail.com>
2018-05-06 15:17:47 -0700

    add jewelry component

---
+++ src/app/Jewelry&Accessories/jewelry.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { Jewelry } from '../jewelry.model';
+
+@Component({
+  selector: 'app-jewelry',
+  templateUrl: './jewelry.component.html',
+  styleUrls: ['./jewelry.component.css']
+})
+export class JewelryComponent implements OnInit {
+
+  }
+
+
+}
