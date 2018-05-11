3645caa835f3565baa0dbcf36324bf9df597da92
bv <bellavanillo@gmail.com>
2018-05-11 11:23:24 -0700

    add firebase to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
 import { AdminComponent } from './admin/admin.component';
@@ -12,7 +13,12 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { ListingDetailComponent } from './listing-detail/listing-detail.component';
 import { ListingsComponent } from './listings/listings.component';
 
-
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 
 @NgModule({
@@ -26,7 +32,11 @@ import { ListingsComponent } from './listings/listings.component';
     ListingsComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    FormsModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
