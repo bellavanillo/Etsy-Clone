32b6c010913b2f6728884fb4a1251b15ec29e5d5
bv <bellavanillo@gmail.com>
2018-05-11 11:11:02 -0700

    add firebase credentials to app, add to git ignore file, and update listings

--- src/app/listings/listings.component.ts
+++ src/app/listings/listings.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../listing.model';
+
 
 @Component({
   selector: 'app-listings',
   templateUrl: './listings.component.html',
-  styleUrls: ['./listings.component.css']
+  styleUrls: ['./listings.component.css'],
+  providers: [ListingService]
 })
+
 export class ListingsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
