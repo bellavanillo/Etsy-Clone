03de06ee9fe9d8b1a11096ceb8bb6b26986a60ea
bv <bellavanillo@gmail.com>
2018-05-11 10:48:37 -0700

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,4 @@
 import { Component } from '@angular/core';
-import { Art } from './models/art.model';
-import { Clothing } from './models/clothing.model';
-import { Craft } from './models/craft.model';
-import { Home } from './models/home.model';
-import { Homepage } from './models/home-page.model';
-import { Jewelry } from './models/jewelry.model';
-import { Toy } from './models/toy.model';
-import { Vintage } from './models/vintage.model';
-import { Wedding } from './models/wedding.model';
-
 
 @Component({
   selector: 'app-root',
@@ -16,16 +6,5 @@ import { Wedding } from './models/wedding.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Etsy App';
-  ArtAndCollectibles: Art [] = [
-    new Art()
-    new Art()
-    new Art()
-    new Art()
-    new Art()
-  ];
-
-  selectedArt = null;
-  
-
- }
+  title = 'Etsy';
+}
